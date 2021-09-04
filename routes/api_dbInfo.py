58af926364581d03b6c6204d6c00338c2a5e794a
erik <erik@iotech.systems>
2021-09-04 15:42:19 +0200

    asdfasdf

--- routes/api_dbInfo.py
+++ routes/api_dbInfo.py
@@ -12,23 +12,24 @@ class api_dbinfo(fr.Resource):
 
    @staticmethod
    def get():
-      jsonBuff = ""; buff = {}
+      jsonBuff = ""; buff = {}; dts = None; connStr = ""
+      dbStatus = "closed"
       try:
          dts = datetime.utcnow().strftime(dtsFormats.std)
-         dbStatus = "closed"
          connStr = open("config/dbconn.string", "r").read().strip()
          # - - - -
          conn: psycopg2.extensions.connection = dbConfig.getConnection()
-         if conn.status == psycopg2.extensions.STATUS_READY:
+         if conn is None:
+            dbStatus = "connIsNone"
+         if (conn is not None) and (conn.status == psycopg2.extensions.STATUS_READY):
             dbStatus = "ready"
             conn.close()
-         # - - - -
-         buff = {"SbmsRestApiServer": dts
-            , "dbConnString": connStr
-            , "dbStatus": dbStatus}
          # - - - -
       except Exception as e:
          buff["exception"] = str(e)
       finally:
+         buff = {"SbmsRestApiServer": dts
+            , "dbConnString": connStr
+            , "dbStatus": dbStatus}
          jsonBuff = json.dumps(buff)
          return flask.Response(jsonBuff, content_type="text/json")
