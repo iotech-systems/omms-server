68f6b69606839d266ebd6fb519f700e829422332
erik <erik@iotech.systems>
2021-09-04 15:06:45 +0200

    fix to dbinfo

--- routes/api_dbInfo.py
+++ routes/api_dbInfo.py
@@ -3,6 +3,8 @@ import flask, json
 from datetime import datetime
 import flask_restful as fr
 from sbmslib.shared.misc import dtsFormats
+from core.data.dbConfig import dbConfig
+import psycopg2.extensions
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -10,13 +12,23 @@ class api_dbinfo(fr.Resource):
 
    @staticmethod
    def get():
+      jsonBuff = ""; buff = {}
       try:
          dts = datetime.utcnow().strftime(dtsFormats.std)
+         dbStatus = "closed"
          connStr = open("config/dbconn.string", "r").read().strip()
-         buff = {"SbmsRestApiServer": dts,
-               "dbConnString": connStr}
          # - - - -
+         conn: psycopg2.extensions.connection = dbConfig.getConnection()
+         if conn.status == psycopg2.extensions.STATUS_READY:
+            dbStatus = "ready"
+            conn.close()
+         # - - - -
+         buff = {"SbmsRestApiServer": dts
+            , "dbConnString": connStr
+            , "dbStatus": dbStatus}
+         # - - - -
+      except Exception as e:
+         buff["exception"] = str(e)
+      finally:
          jsonBuff = json.dumps(buff)
          return flask.Response(jsonBuff, content_type="text/json")
-      except Exception as e:
-         return {f"exception: {str(e)}"}
