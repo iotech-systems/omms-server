d3888251a5258a560b03b02a82fb754477a61940
erik <erik@iotech.systems>
2021-09-07 21:32:36 +0200

    added streamTbl tag going back to the client

--- routes/api_streamer.py
+++ routes/api_streamer.py
@@ -1,4 +1,5 @@
 
+import logging
 import codecs, flask, json
 import flask_restful as fr
 from sbmslib.shared.utils.jsonx import jsonx
@@ -27,16 +28,23 @@ class api_streamer(fr.Resource):
 
    @staticmethod
    def get():
-      jsonStr = None
+      status = 400
+      jsonStr = "{\"Error\": \"MissingInput\"}"
       try:
+         badInput = (None, "None")
          streamTbl: str = str(flask.request.args.get("streamTbl"))
          meterDBID: str = str(flask.request.args.get("meterDBID"))
+         if (streamTbl in badInput) or (meterDBID in badInput):
+            raise Exception("MissingInput")
          db: dbOps.databaseOps = dbOps.databaseOps()
          resObj = db.read_lastFromStreamTbl(streamTbl, meterDBID)
+         # tag returning data packet
+         resObj["streamTbl"] = streamTbl
          if resObj is not None:
             jsonStr = json.dumps(resObj)
+            status = 200
       except Exception as e:
-         print(e)
+         logging.error(e)
       finally:
-         return flask.Response(response=jsonStr, status=200
+         return flask.Response(response=jsonStr, status=status
             , content_type="application/json")
