89eb0fdef8029c94194fb5fb711b178267b61252
erik <erik@iotech.systems>
2021-09-20 22:19:32 +0200

    debug trace

--- routes/api_streamer.py
+++ routes/api_streamer.py
@@ -16,6 +16,7 @@ class api_streamer(fr.Resource):
          data = fr.request.data
          # json string
          jsonStr = codecs.decode(data, "UTF-8")
+         print(jsonStr)
          # create model
          jObj = jsonx.getJsonObj(jsonStr)
          database = dbOps.databaseOps()
