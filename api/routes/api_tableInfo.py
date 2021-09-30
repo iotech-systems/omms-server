cda675ce76ea97de926cdc700109893ad9d7ab43
erik <erik@iotech.systems>
2021-09-30 21:47:18 +0200

    tbl work

--- api/routes/api_tableInfo.py
+++ api/routes/api_tableInfo.py
@@ -6,13 +6,17 @@ import core.data.databaseOps as dbOps
 from routes.api_flask import api_flask
 
 
+TBLS = {"0x00": "clients", "0x02": "circuits", "0x04": "meters"}
+
+
 class api_tableInfo(fr.Resource):
 
    @staticmethod
    def get():
       try:
          jsonStr = ""
-         tableName: str = str(flask.request.args.get("tableName"))
+         IDX: str = str(flask.request.args.get("tblidx"))
+         tableName = TBLS[IDX]
          db: dbOps.databaseOps = dbOps.databaseOps()
          resObj = db.get_tableInfo(tableName)
          if resObj is not None:
