0652f5e907fc75ef4ed94fa32eb3c221eb383ab2
erik <erik@iotech.systems>
2021-10-07 00:41:47 +0200

    clients sql

--- api/core/data/configSQL.py
+++ api/core/data/configSQL.py
@@ -9,5 +9,14 @@ class configSQL(object):
          dbid = int(dataDict["meter_dbid"])
          ctag = dataDict["circuit_tag"]
          qry = f"update config.meters set circuit_tag = '{ctag}' where meter_dbid = {dbid};"
+      if tblname == "clients":
+         dbid = dataDict["client_dbid"]
+         tag = dataDict["client_tag"]
+         name = dataDict["client_name"]
+         if "default" in dbid:
+            qry = f"insert into reports.clients values(default, '{tag}', '{name}');"
+         else:
+            qry = f"update reports.client set client_tag = '{tag}', client_name = '{name}' " \
+               f" where client_dbid = {dbid};"
       # -- return qry --
       return qry
