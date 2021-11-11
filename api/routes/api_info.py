2a7be6dc2e3416a5bce6fd4201dbefbf09a7b41e
erik <erik@iotech.systems>
2021-11-11 13:05:43 +0100

    api info update

--- api/routes/api_info.py
+++ api/routes/api_info.py
@@ -1,7 +1,11 @@
-
+import json
 from datetime import datetime
 import flask_restful as fr
+from routes.api_flask import api_flask
 from ommslib.shared.misc import dtsFormats
+
+app = "omms-api"
+ver = "0.1.0"
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -11,7 +15,7 @@ class api_info(fr.Resource):
    def get():
       try:
          dts = datetime.utcnow().strftime(dtsFormats.std)
-         buff = f"sbms-rest-api-server: {dts}"
-         return buff
+         out = {"app": app, "ver": ver, "dts": dts}
+         return api_flask.jsonResp(json.dumps(out), 200)
       except Exception as e:
          return {f"exception: {str(e)}"}
