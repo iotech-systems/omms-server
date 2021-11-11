f0980b546ee69443f1a108ece3407750314cfe02
erik <erik@iotech.systems>
2021-11-11 01:53:52 +0100

    sql fix

--- api/core/data/configSQL.py
+++ api/core/data/configSQL.py
@@ -46,13 +46,16 @@ class configSQL(object):
 
    @staticmethod
    def activeElectricMeters(elcRoomTag: str):
+      """
+         this sql goes into json sql call ... can't to ; on the sql code
+      """
       # return f"select distinct(bps.fk_meter_dbid) from streams.\"__basic_pwr_stats\" bps " \
       #    f" join streams.\"__kwhrs\" k on bps.fk_meter_dbid = k.fk_meter_dbid " \
       #    f" join config.meters m on m.meter_dbid = bps.fk_meter_dbid " \
       #    f" where m.elcrm_entag = '{elcRoomTag}'"
       return f"select distinct(am.fk_meter_dbid) from \"diagnostics\".active_meters am"\
-         f" join config.meters m  on m.meter_dbid = am.fk_meter_dbid"\
-         f" where m.elcrm_entag  = '{elcRoomTag}';"
+         f" join config.meters m on m.meter_dbid = am.fk_meter_dbid"\
+         f" where m.elcrm_entag = '{elcRoomTag}'"
 
    @staticmethod
    def __meters__(dataDict: []):
